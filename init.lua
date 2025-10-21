86c4a2cc5a5efbaa3f4721fac21b6e431f9a31d7
Joel Kuehne <joelckuehne@gmail.com>
2025-10-21 16:08:13 -0500

    Removed treesitter autocommand for disabling on verilog and systemverilog

--- init.lua
+++ init.lua
@@ -1,10 +1,3 @@
 require("config.lazy")
 checker = { enabled = true, notify = false }
 news = {lazyvim = false}
-
-vim.api.nvim_create_autocmd("FileType", {
-  pattern = "verilog,systemverilog",
-  callback = function()
-    vim.cmd("TSDisable highlight")
-  end,
-})
