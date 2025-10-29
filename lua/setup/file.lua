6c8fc5c4d1c99b3dc27c0832c1c3a6b20f5e2098
Joel Kuehne <joelckuehne@gmail.com>
2025-10-29 09:24:44 -0500

    Updates to telescope UI

--- lua/setup/file.lua
+++ lua/setup/file.lua
@@ -81,6 +81,31 @@ function M.setup_telescope()
     local telescope = require("telescope")
     local builtin = require("telescope.builtin")
     telescope.load_extension("undo")
+
+    telescope.setup({
+        defaults = {
+            preview = { treesitter = false },
+            color_devicons = true,
+            sorting_strategy = "ascending",
+            borderchars = {
+                "─", -- top
+                "│", -- right
+                "─", -- bottom
+                "│", -- left
+                "┌", -- top-left
+                "┐", -- top-right
+                "┘", -- bottom-right
+                "└", -- bottom-left
+            },
+            path_displays = { "smart" },
+            layout_config = {
+                height = 100,
+                width = 400,
+                prompt_position = "top",
+                preview_cutoff = 40,
+            }
+        }
+    })
 end
 
 function M.attach_gitsigns(buffer)
