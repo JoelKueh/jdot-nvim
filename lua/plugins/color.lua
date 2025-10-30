d1256996427541f18a3c3b7c78922051a0e8cb07
Joel Kuehne <joelckuehne@gmail.com>
2025-10-30 11:26:36 -0500

    Updates to colorschemes

--- lua/plugins/color.lua
+++ lua/plugins/color.lua
@@ -1,45 +1,57 @@
 
 return {
-	{
-		"gbprod/nord.nvim",
-		event = "VeryLazy",
-		config = function()
-			require("nord").setup({
-				transparent = true
-			})
-		end
-	},
-	{
-		"neanias/everforest-nvim",
-		event = "VeryLazy"
-	},
-	{
-		"sainnhe/sonokai",
-		event = "VeryLazy"
-	},
-	{
-		"catppuccin/nvim",
-                
-		lazy = false,
-		priority = 1000,
-		config = function()
-			vim.cmd.colorscheme("catppuccin-mocha")
-		end
-	},
-	{
-		"Mofiqul/vscode.nvim",
-		event = "VeryLazy"
-	},
-	{
-		"sainnhe/gruvbox-material",
-		event = "VeryLazy"
-	},
-	{
-		"fneu/breezy",
-		event = "VeryLazy"
-	},
-	{
-		"folke/tokyonight.nvim",
-		event = "VeryLazy"
-	}
+    {
+        "gbprod/nord.nvim",
+        event = "VeryLazy",
+        config = function()
+            require("nord").setup({
+                transparent = true
+            })
+        end
+    },
+    {
+        "neanias/everforest-nvim",
+        event = "VeryLazy"
+    },
+    {
+        "sainnhe/sonokai",
+        event = "VeryLazy"
+    },
+    {
+        "catppuccin/nvim",
+
+        lazy = false,
+        priority = 1000,
+        config = function()
+            vim.cmd.colorscheme("catppuccin-mocha")
+        end
+    },
+    {
+        "Mofiqul/vscode.nvim",
+        event = "VeryLazy"
+    },
+    {
+        "sainnhe/gruvbox-material",
+        event = "VeryLazy"
+    },
+    {
+        "fneu/breezy",
+        event = "VeryLazy"
+    },
+    {
+        "folke/tokyonight.nvim",
+        event = "VeryLazy"
+    },
+    {
+        "savq/melange-nvim",
+        event = "VeryLazy"
+    },
+    {
+        "rebelot/kanagawa.nvim",
+        event = "VeryLazy"
+    },
+    {
+        "vague-theme/vague.nvim",
+        event = "VeryLazy"
+    },
 }
